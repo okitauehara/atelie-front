9755510c49e27df0104d1b8d7fa57f003b98e6a2
okitauehara <marcosokita@live.com>
2021-11-07 22:51:39 -0300

    Base layout of signup page finished

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,26 @@
+import {
+  PageStyle,
+  Logo,
+  Form,
+  Input,
+  Button,
+  Redirect,
+} from '../styles/LoginSignUpStyle';
+import logo from '../assets/logo.png';
+
 function SignUp() {
   return (
-    <h1>SignUp</h1>
+    <PageStyle>
+      <Logo src={logo} alt="Logo Atelie" />
+      <Form>
+        <Input placeholder="Nome" />
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
+        <Input placeholder="Confirme a senha" />
+        <Button>Cadastrar</Button>
+      </Form>
+      <Redirect>Já tem cadastro? Faça login e aproveite!</Redirect>
+    </PageStyle>
   );
 }
 

ca8cb987a4bc8adbde98910cc4ede5ec58a1459f
okitauehara <marcosokita@live.com>
2021-11-08 14:31:25 -0300

    Front SignUp functions finished

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,18 +1,48 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import * as S from '../styles/LoginSignUpStyle';
 import logo from '../assets/logo.png';
+import { postSignUp } from '../services/API';
 
 function SignUp() {
   const [inputData, setInputData] = useState({ name: '', email: '', password: '' });
+  const [isDisabled, setIsDisabled] = useState(false);
+  const navigate = useNavigate();
   const emailRegex = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}';
 
   const handleChange = (event) => {
     setInputData({ ...inputData, [event.target.name]: event.target.value });
   };
 
-  const submitSignUp = () => {
-    // Implementar API de Login
+  const submitSignUp = (event) => {
+    event.preventDefault();
+    setIsDisabled(true);
+    const body = inputData;
+    postSignUp(body)
+      .then(async () => {
+        await Swal.fire({
+          icon: 'success',
+          title: 'Usuário cadastrado!',
+        });
+        setIsDisabled(false);
+        navigate('/');
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Verifique se todos os dados inseridos são válidos',
+          });
+        }
+        if (err.response.status === 409) {
+          Swal.fire({
+            icon: 'error',
+            title: 'O e-mail inserido já está em uso',
+          });
+        }
+        setIsDisabled(false);
+      });
   };
 
   return (
@@ -28,6 +58,7 @@ function SignUp() {
           onChange={handleChange}
           minLength="3"
           autoFocus
+          disabled={isDisabled}
         />
         <S.Input
           required
@@ -37,6 +68,7 @@ function SignUp() {
           value={inputData.email}
           onChange={handleChange}
           pattern={emailRegex}
+          disabled={isDisabled}
         />
         <S.Input
           required
@@ -46,16 +78,18 @@ function SignUp() {
           value={inputData.password}
           onChange={handleChange}
           minLength="8"
+          disabled={isDisabled}
         />
         <S.Input
           required
           placeholder="Confirme a senha"
           type="password"
           pattern={inputData.password}
+          disabled={isDisabled}
         />
-        <S.Button type="submit">Cadastrar</S.Button>
+        <S.Button type="submit" disabled={isDisabled}>Cadastrar</S.Button>
       </S.Form>
-      <Link to="/">
+      <Link to="/" disabled={isDisabled}>
         <S.Redirect>Já tem cadastro? Faça login e aproveite!</S.Redirect>
       </Link>
     </S.PageStyle>

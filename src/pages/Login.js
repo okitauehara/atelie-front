dd6b1e20a2684f57275980b9cde0adb35d1be62a
okitauehara <marcosokita@live.com>
2021-11-09 12:02:05 -0300

    Base login page layout finished

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,20 @@
+import { Link } from 'react-router-dom';
+import * as S from '../styles/LoginSignUpStyle';
+import logo from '../assets/logo.png';
 
 function Login() {
   return (
+    <S.PageStyle>
+      <S.Logo src={logo} alt="Logo Atelie" />
+      <S.Form>
+        <S.Input placeholder="E-mail" />
+        <S.Input placeholder="Senha" />
+        <S.Button type="submit">Entrar</S.Button>
+      </S.Form>
+      <Link to="/sign-up">
+        <S.Redirect>Não tem uma conta? Clique e cadastre-se.</S.Redirect>
+      </Link>
+    </S.PageStyle>
   );
 }
 
